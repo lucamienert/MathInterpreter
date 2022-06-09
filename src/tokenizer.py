430e0ad4e2697baf2adc8e7f3c54aba48fd98ac6
Luca Mienert <67235946+lucamienert@users.noreply.github.com>
2022-06-09 14:54:21 +0200

    Update Tokenizer

--- src/tokenizer.py
+++ src/tokenizer.py
@@ -13,43 +13,43 @@ class Tokenizer:
 
     def generate_tokens(self):
         while self.current != None:
-            if self.current_char in ' \n\t':
+            if self.current in ' \n\t':
                 self.advance()
-            elif self.current_char == '.' or self.current_char in '0123456789':
+            elif self.current == '.' or self.current in '0123456789':
                 yield self.generate_number()
-            elif self.current_char == '+':
+            elif self.current == '+':
                 self.advance()
                 yield Token(Type.PLUS)
-            elif self.current_char == '-':
+            elif self.current == '-':
                 self.advance()
                 yield Token(Type.MINUS)
-            elif self.current_char == '*':
+            elif self.current == '*':
                 self.advance()
                 yield Token(Type.MULTIPLY)
-            elif self.current_char == '/':
+            elif self.current == '/':
                 self.advance()
                 yield Token(Type.DIVIDE)
-            elif self.current_char == '(':
+            elif self.current == '(':
                 self.advance()
                 yield Token(Type.LPAREN)
-            elif self.current_char == ')':
+            elif self.current == ')':
                 self.advance()
                 yield Token(Type.RPAREN)
             else:
-                raise Exception(f"Illegal character '{self.current_char}'")
+                raise Exception(f"Illegal character '{self.current}'")
 
     def generate_number(self):
         decimal_point_count = 0
-        number_str = self.current_char
+        number_str = self.current
         self.advance()
 
-        while self.current_char != None and (self.current_char == '.' or self.current_char in '0123456789'):
-            if self.current_char == '.':
+        while self.current != None and (self.current == '.' or self.current in '0123456789'):
+            if self.current == '.':
                 decimal_point_count += 1
                 if decimal_point_count > 1:
                     break
 
-            number_str += self.current_char
+            number_str += self.current
             self.advance()
 
         if number_str.startswith('.'):
